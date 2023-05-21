cc0a7c3f414ae2345eb26a72865dbca301c7d506
HarshP34 <hupatel1005@gmail.com>
2023-05-21 12:51:04 +0530

    basic about react element & component

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-        [
-            React.createElement("h1", {}, "I'm a h1 tag"),
-            React.createElement("h2", {}, "I'm a h2 tag")
-        ])
-)
+// JSX
+const Title = () => (
+  <h1 className="head" tabIndex={1}>
+    Namste React Using JSX🚀
+  </h1>
+);
+
+const elem = <span>React element</span>;
+
+const data = 1000;
+
+//React Component
+
+//Functional Component = A function that returns jsx code or react elements that is called functional component.
+
+//Component Composition(Component inside Component)
+const HeadingComponent = () => (
+  <>
+    {/* React Fragmentation */}
+    <div id="container">
+      {Title()} {/* React COmponent writting methods*/}
+      <Title />
+      <h1 className="heading">Namste React Functional Component</h1>
+    </div>
+    <div id="container 2">
+      <Title></Title>
+      <h1 id="title 2"> Title 2</h1>
+    </div>
+  </>
+);
+
+//React Fragments = If we have to use two div in the jsx then we use it
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-#React Basics👩‍🚀
+# React Basics👩‍🚀
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm
+- Caching Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consitent Hashing
+- Code Splitting
+- Differential Bundling
